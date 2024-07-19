752595f1c0cfaadeb9c2f15622e5b69b405536e3
Susmitha Reddy <susmitha.me@hotmail.com>
2024-07-19 22:03:24 +0530

    Revert "Merge branch 'master' of https://github.com/susmitha1103/SmartStore"
    This reverts commit 3621cdf48b0e9b8f261d7dcb424c1a719ce68f16, reversing
    changes made to 4b58f0b282de60bcce448f0870ff17677c57048d.
    
    Updated the project structure by creating a backend directory and moved
    all the backend files

--- backend/routes/productManagement.js
+++ backend/routes/productManagement.js
@@ -31,7 +31,7 @@ router.get('/:id', async(req,res) =>{
   try{
     const product = await PRODUCT.findById(req.params.id);
     if(!product){
-      return res.status(404).json({msg: 'Product not found'});
+      return res.status(404).json({msg: 'Product not found'})
     }
     res.json(product);
   }
@@ -42,43 +42,6 @@ router.get('/:id', async(req,res) =>{
     }
     res.status(500).send('Server error');
   }
-});
-
-router.post('/update/:id',adminAuthentication, async(req,res)=>{
-  const { product_name, category, description, price, stock, image } = req.body;
-  try{
-    const product = await PRODUCT.findById(req.params.id);
-    if(!product){
-      return res.status(404).json({msg: 'product not found'});
-    }
-    //update the products
-    product.product_name = product_name || product.product_name;
-    product.category = category || product.category;
-    product.description = description || product.description;
-    product.price = price || product.price;
-    product.stock = stock || product.stock;
-    product.Image = image || product.Image;
-
-    // Save the updated product
-    await product.save();
-
-    res.json({ msg: 'Product updated successfully', product });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  } 
-});
-router.delete('/removeProduct/:id', adminAuthentication, async (req, res) => {
-  try {
-    const product = await PRODUCT.findByIdAndDelete(req.params.id);
-    if (!product) {
-      return res.status(404).json({ msg: 'Product not found' });
-    }
-    res.json({ msg: 'Product removed successfully' });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-});
+})
 
 module.exports = router;
