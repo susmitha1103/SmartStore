97f068738efe73af594dd47b886b74f9141f1d0c
Susmitha Reddy <susmitha.me@hotmail.com>
2024-09-10 15:41:55 +0530

    Removed unnecessary components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import AdminLogin from './Components/Admin/AdminLogin';
 import AdminSignUp from './Components/Admin/AdminSignUp';
 import Appbar from './Components/Appbar';
 import AddProduct from './Components/Admin/AddProduct';
-import UpdateProductDetails from './Components/Admin/UpdateProductDetails';
-import ProductList from './Components/ProductList';
-import DeleteProduct from './Components/DeleteProduct';
-import AdminDashboard from './Components/AdminDashboard';
 import ProductListing from './Components/ProductListing';
-
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
 
   useEffect(() => {
     const token = localStorage.getItem('jwtToken');
@@ -26,21 +20,20 @@ const App = () => {
     }
   }, []);
 
+  
+  const ProtectedRoute = ({ element }) => {
+    return isLoggedIn ? element : <Navigate to="/login" />;
+  };
+
   return (
     <Router>
-     <Appbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-     <Routes>
-     <Route exact path="/products" component={ProductListing} />    
-  <Route path="/signup" element={<AdminSignUp />} />
-  <Route path="/login" element={<AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
-  <Route path="/add-product" element={isLoggedIn ? <AddProduct /> : <AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
-  <Route path="/update-product/:id" element={isLoggedIn ? <UpdateProductDetails /> : <AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
-  <Route path="/products" element={isLoggedIn ? <ProductList /> : <AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
-  <Route path="/delete-product" element={isLoggedIn ? <DeleteProduct /> : <AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
-  <Route path="/AdminDashboard" element={isLoggedIn ? <AdminDashboard /> : <AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
-  <Route path = "/view-products" element = {isLoggedIn ? <ProductListing/> : <AdminLogin setIsLoggedIn = {setIsLoggedIn} />} />
-</Routes>
-
+      <Appbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route path="/view-products" element={<ProtectedRoute element={<ProductListing />} />} />
+        <Route path="/signup" element={<AdminSignUp />} />
+        <Route path="/login" element={<AdminLogin setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/add-product" element={<ProtectedRoute element={<AddProduct />} />} />
+      </Routes>
     </Router>
   );
 };
